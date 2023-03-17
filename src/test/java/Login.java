f04e60aaa8560eb20ec234dbd58ef1a60d1f881f
Amar Gupta <amar@Amars-MacBook-Air.local>
2023-03-17 21:02:57 +0530

    Basic launch and Close browser

---
+++ src/test/java/Login.java
@@ -0,0 +1,11 @@
+import Utils.WebSetup;
+import org.testng.annotations.Test;
+
+public class Login extends WebSetup {
+
+    @Test
+    public void LoginHere(){
+        System.out.println("Hello");
+
+    }
+}
